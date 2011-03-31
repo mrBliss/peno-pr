0c39d680eda8a36c29c28863c3dcd62683eb514e
fly.jdv@gmail.com <fly.jdv@gmail.com@0719f6ee-27ed-6e14-fccf-f9959a5ee2ed>
2011-03-31 16:33:06 +0000

    new ConfigTest program for testing the RobotBTGWconfigurator

--- geel_Robot/trunk/src/geel/ConfigTest.java
+++ geel_Robot/trunk/src/geel/ConfigTest.java
@@ -2,17 +2,12 @@ package geel;
 
 import geel.BTGW.infrastructure.BTGWConnection;
 import geel.BTGW.infrastructure.BTGateway;
-import geel.BTGW.infrastructure.IBTGWCommandListener;
-import geel.BTGW.packets.BTGWPacket;
-import geel.BTGW.packets.BTGWPacketConfigBoolean;
-import geel.BTGW.packets.BTGWPacketConfigFloat;
-import geel.BTGW.packets.BTGWPacketConfigInteger;
-import geel.BTGW.packets.BTGWPacketConfigRequest;
 import geel.BTGW.robot.BTGWRealConnectionTaker;
 
 /**
- * short config test that prints out if a Config packets has been received
- * and sends a bogus speed configuration on a config request
+ * short test program to test the {@link RobotBTGWConfigurator} 
+ * using a bogus Configurable object with 2 int parameters, one float and one boolean parameter
+ * 
  * 
  * 
  * @author jeroendv
@@ -24,35 +19,13 @@ public class ConfigTest {
 	public static void main(String[] args) {
 		initilizeBTGateWay();
 		
+		RobotBTGWConfigurator.registerWithBTGW();
 		
-		BTGateway.getInstance().addOmniListener(new IBTGWCommandListener() {
-			
-			@Override
-			public void handlePacket(BTGWPacket packet) {
-				if(packet instanceof BTGWPacketConfigBoolean){
-					System.out.println("received: BTGWPacketConfigBoolean");
-				}
-				
-				if(packet instanceof BTGWPacketConfigFloat){
-					System.out.println("received: BTGWPacketConfigFloat");
-				}
-				
-				if(packet instanceof BTGWPacketConfigInteger){
-					System.out.println("received: BTGWPacketConfigInterger");
-				}
-				
-				if(packet instanceof BTGWPacketConfigRequest){
-					System.out.println("received: BTGWPacketConfigRequest");
-					BTGateway.getInstance().sendPacket(new BTGWPacketConfigInteger("speed",100));
-				}
-			}
-		});
-		
-		
+		RobotBTGWConfigurator.register(bogusConfigurableObject);
 		
 	}
-	
-	
+
+
 	/**
 	 * initialize a Bluetooth Gateway for communication between robot and pc and
 	 * make it globally accessible through BTGateWay.getinstance()
@@ -76,5 +49,103 @@ public class ConfigTest {
 		// make the BTGW globally accessible through BTGateWay.getinstance()
 		BTGateway.addInstance(new BTGateway(btconn));
 	}
+	
+	
+	private static Configurable bogusConfigurableObject = new Configurable() {
+		
+		private final String testBoolId = "testBoolId";
+		private boolean testBool = false;
+		
+		private final String testIntId1 = "testIntId1";
+		private int testInt1 = 0;
+		
+		private final String testIntId2 = "testIntId2";
+		private int testInt2 = 0;
+		
+		private final String testFloatId = "testFloatId";
+		private float testFloat = 0;
+		
+		@Override
+		public void setConfigurableInt(String id, int value)
+				throws IllegalArgumentException {
+			if(id.equals(testIntId1)){
+				this.testInt1 = value;
+			}else if(id.equals(testIntId2)){
+				this.testInt2 = value;
+			}
+			else{
+				throw new IllegalArgumentException("parameter '"+id+"' doesn't exists");
+			}
+			
+		}
+		
+		@Override
+		public void setConfigurableFloat(String id, float value)
+				throws IllegalArgumentException {
+			if(id.equals(testFloatId)){
+				this.testFloat = value;
+			}else{
+				throw new IllegalArgumentException("parameter '"+id+"' doesn't exists");
+			}
+			
+		}
+		
+		@Override
+		public void setConfigurableBoolean(String id, boolean value)
+				throws IllegalArgumentException {
+			if(id.equals(testBoolId)){
+				this.testBool = value;
+			}else{
+				throw new IllegalArgumentException("parameter '"+id+"' doesn't exists");
+			}
+			
+		}
+		
+		@Override
+		public String[] listConfigurableIntegers() {
+			return new String[]{testIntId1,testIntId2};
+		}
+		
+		@Override
+		public String[] listConfigurableFloat() {
+			return new String[]{testFloatId};
+		}
+		
+		@Override
+		public String[] listConfigurableBooleans() {
+			return new String[]{testBoolId};
+		}
+		
+		@Override
+		public int getConfigurableInt(String id) throws IllegalArgumentException {
+			if(id.equals(testIntId1)){
+				return this.testInt1;
+			}else if(id.equals(testIntId2)){
+				return this.testInt2;
+			}else{
+				throw new IllegalArgumentException("parameter '"+id+"' doesn't exists");
+			}
+		}
+		
+		@Override
+		public float getConfigurableFloat(String id)
+				throws IllegalArgumentException {
+			if(id.equals(testFloatId)){
+				return this.testFloat;
+			}else{
+				throw new IllegalArgumentException("parameter '"+id+"' doesn't exists");
+			}
+		}
+		
+		@Override
+		public boolean getConfigurableBoolean(String id)
+				throws IllegalArgumentException {
+			if(id.equals(testBoolId)){
+				return this.testBool;
+			}else{
+				throw new IllegalArgumentException("parameter '"+id+"' doesn't exists");
+			}
+		}
+	};
 
 }

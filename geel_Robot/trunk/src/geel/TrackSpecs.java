6b0b72706677688dc516961defcf9dfb8370841e
fly.jdv@gmail.com <fly.jdv@gmail.com@0719f6ee-27ed-6e14-fccf-f9959a5ee2ed>
2011-04-04 14:28:11 +0000

    added a barcode class and a decoded class

--- geel_Robot/trunk/src/geel/TrackSpecs.java
+++ geel_Robot/trunk/src/geel/TrackSpecs.java
@@ -1,10 +1,17 @@
 package geel;
 
+import geel.barcodes.Barcode;
+import geel.barcodes.BarcodeDecoder;
+
 import java.util.Hashtable;
 
+/**
+ * @author jeroendv
+ *
+ */
 public class TrackSpecs {
 	
-	/*
+	/**
 	 * integer codes for the 3 different colors identified by the robot
 	 */
 	public static final int BLACK_COLOR = 0;
@@ -23,62 +30,31 @@ public class TrackSpecs {
      */
     public static final int barcodeBitWitdth = 2;
     
-//    FIXME: is this list correct?
-    public static final int[] barcodes = new int[]{
-    	0x00,
-    	0x0f,
-    	0x15,
-    	0x19,
-    	0x25,
-    	0x2A,
-    	0x33,
-		0x3C,
-		0x43,
-		0x4C,
-		0x55,
-		0x5A,
-		0x66,
-		0x69,
-		0x70,
-		0x7f,
-    };
     
-    //TODO
-	public static final int TurnRighBarcode = 0x33;
+    /**
+     * barcode specification mapping barcode color to a bit value 
+     *  0 = black
+     *  1 = white
+     */
+    public static final byte  BLACK_BAR_VALUE = 0;
+    public static final byte  WHITE_BAR_VALUE = 1;
+    
+    
+    /**
+     * 
+     */
+	public static final Barcode TurnRighBarcode = BarcodeDecoder.VALID_BARCODES[6];
 	 //TODO
-	public static final int TurnLeftBarcode = 0x19;
+	public static final Barcode TurnLeftBarcode = BarcodeDecoder.VALID_BARCODES[3];
 	//TODO
-	public static final int GoForwardBarcode = 0;
+	public static final Barcode GoForwardBarcode = BarcodeDecoder.VALID_BARCODES[1];
 	
 	
     
-    public static boolean isBarcodeValid(int barcode){
-    	for(int i = 0; i >16; i++){
-    		if(barcodes[i] == barcode){
-    			return true;
-    		}
-    	}
-    	return false;
-    }
+
     
     
-    //FIXME
-    public static String barcodeToString(int barcode){
-    	char[] charArray = new char[7];
-    	
-    	int mask = 0x01;
-    	for(int i =0; i < 7 ; i ++){
-    		int bitValue = barcode & mask;
-    		if(bitValue != 0){
-    			charArray[i]= '1';
-    		}else{
-    			charArray[i] = '0';
-    		}
-    		mask = mask <<1;
-    	}
-    	
-    	return new String(charArray);
-    	
-    }
+    
+
 
 }
---
+++ geel_Robot/trunk/src/geel/barcodes/BarcodeDecoder.java
@@ -0,0 +1,86 @@
+package geel.barcodes;
+
+/**
+ * This class uses 7 bit barcodes to encode 4 bits of information
+ * with 3 bits redundancy.
+ * 
+ * the encoding scheme is constructed such that any 7 bit sequence
+ * has at most a hamming distance of 1 to a valid bit sequence.
+ * Thus allowing the correction of a one bit error in an unknown position.
+ * 
+ * 
+ * 
+ * @author jeroendv
+ *
+ */
+public class BarcodeDecoder {
+	
+    
+    /**
+     * an array of the 16 valid bit sequences
+     */
+    public static final Barcode[] VALID_BARCODES = new Barcode[]{
+    	new Barcode("0000000"), // 0
+    	new Barcode("0001111"), // 1
+    	new Barcode("0010110"), // 2
+    	new Barcode("0011001"), // 3
+    	new Barcode("0100101"), // 4
+    	new Barcode("0101010"), // 5
+    	new Barcode("0110011"), // 6
+    	new Barcode("0111100"), // 7
+    	new Barcode("1000011"), // 8
+    	new Barcode("1001100"), // 9
+    	new Barcode("1010101"), // A 10
+    	new Barcode("1011010"), // B 11
+    	new Barcode("1100110"), // C 12
+    	new Barcode("1101001"), // D 13
+    	new Barcode("1110000"), // E 14
+    	new Barcode("1111111"), // F 15
+    };	
+	
+    /**
+     * check if a certain bit sequence is valid
+     * 
+     * @param barcode 
+     * @return true 
+     */
+    public static boolean isBarcodeValid(Barcode barcode){
+    	for(byte i = 0; i < BarcodeDecoder.VALID_BARCODES.length; i++){
+    		if( BarcodeDecoder.VALID_BARCODES[i].equals(barcode) ){
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    
+    
+    /**
+     * nearest neighbor decoder,
+     * which maps any of the 128 possible 7 bit sequences to 
+     * one of the valid barcodes though a one bit change.
+     * 
+     * @param barcode
+     * @return
+     */
+    public static Barcode nearestNeighbor(Barcode barcode){
+    	if(isBarcodeValid(barcode)){
+    		return barcode;
+    	}else{
+    		for( int i=0 ; i<7 ; i++){
+    			Barcode perturbedBarcode = new Barcode(barcode);
+    			perturbedBarcode.flipBit(i);
+    			if(isBarcodeValid(perturbedBarcode)){
+    				return perturbedBarcode;
+    			}
+    		}
+    	}
+    	
+    	throw new IllegalStateException("there must be a bug! this should never happen");
+    }
+    
+    
+	
+	
+
+}
